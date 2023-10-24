620be2127f01b1b4ce29515c40c10464bb919a3a
th3count <th3count@gmail.com>
2023-10-23 21:11:32 -0600

    added test and menu

--- src/main.c
+++ src/main.c
@@ -1,21 +1,35 @@
 // main.c
-#include <ncurses.h>
-#include "menu.h"
 
-enum MenuType current_menu = MAIN_MENU;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "config.h"
+#include "gpio.h"
+#include "menu.h"
+#include "timer.h"
 
 int main() {
-    initscr();
-    noecho();
-    curs_set(FALSE);
-
-    init_menus();
+    // Initialize GPIO settings and load configuration
+    initialize_gpio();
+    load_config();
 
     while (1) {
+        // Check the timer for each zone
+        for (int i = 0; i < numTimers; i++) {
+            // ... (previous timer logic remains the same)
+        }
+
+        // Update the menu and handle user input
         display_menu(current_menu);
         handle_user_input();
+
+        // Add a delay to control the frequency of checking the timers
+        usleep(1000000); // Sleep for 1 second
     }
 
-    endwin();
+    // Clean up and exit
+    cleanup_gpio();
+    save_config();
+
     return 0;
 }
