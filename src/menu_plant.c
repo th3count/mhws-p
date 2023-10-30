4e815b94d575843862c01bc2b62050c8037d3de8
th3count <th3count@gmail.com>
2023-10-29 19:53:57 -0600

    menu examples

---
+++ src/menu_plant.c
@@ -0,0 +1,18 @@
++-------------------------------------------------------+
+| Current Time: 12:45 PM   Date: 2023-10-29   GPS: (37.7749, -122.4194) |
+| Sunrise: 07:15 AM   Sunset: 06:30 PM                     |
++-------------------------------------------------------+
+| Zone Information                                      |
++-------------------------------------------------------+
+| Zone ID | Alias       | Relay Channel | Schedule     |
+|         |             |               |              |
+|   1[-]  | Front Yard  | 5             | D-2, 10 mins |
+|         |             |               |              |
+|         |             | Emitter Size/Recommended: | Water Requirement/Actual:      | Emitter Responsibility  
+|       - Tulips        | 1.5/1 gp/h     			 | 3/2 in						 | 3 SqFt     
+|		- Roses         | 2.0/3 gp/h   				 | 2/3 in						 | 2 SqFt 
+|   2[+]  | Garden      | 2             | W-MWFSu, 15 mins |     
+|   3[+]  | Backyard    | 1             | M-1,14, 12 mins |
++-------------------------------------------------------+
+| Add | Main Menu |
++-------------------------------------------------------+
